b353bfe40261cca6a18d94bdbc95cceed89c66c9
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-07-27 22:02:01 -0400

    commit tree b1b44de7be59e28fe95e8e1fe307f63163a6c8cd

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function writeCards( Array, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < Array.length; i++ ) {
+      thankYouCards.push( `Thank you, ${Array[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+  
+
+ 
+  function countDown( integer ) {
+      while (integer > 0) {
+          console.log( integer );
+          integer -= 1;
+      }
+      console.log(integer);
+  }
+
+//   function countDown( startingNumber ) {
+//     while ( startingNumber > 0 ) {
+//       console.log( startingNumber );
+//       startingNumber -= 1;
+//     }
+//     console.log( startingNumber );
+//   }
